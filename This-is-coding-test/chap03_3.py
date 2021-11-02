d2f2f5c721ca8e62202ce75caf4ff8940bbd5d63
randallkk <randallkk@sookmyung.ac.kr>
2021-11-03 01:46:49 +0900

    211103

--- This-is-coding-test/chap03_3.py
+++ This-is-coding-test/chap03_3.py
@@ -1,16 +1,16 @@
+import time
+
 def solve():
+    start = time.time()
     n, m = map(int, input().split())
     maxcard = 0
     for i in range(n):
         card = sorted(map(int, input().split()))[0]
         if maxcard < card:
             maxcard = card 
-    print(maxcard)
-
-def answer_min():
-    answer = 0
-    print(answer)
+    end = time.time()
+    print("solve", maxcard)
+    print("Time:", end-start)
 
 if __name__=="__main__":
     solve()
-    greedy()
--- This-is-coding-test/chap03_4.py
+++ This-is-coding-test/chap03_4.py
@@ -1,5 +1,20 @@
-def solution():
-    print()
+import time
+
+def solve(n, k):
+    start = time.time()
+    answer = 0
+    while True:
+        if n == 1:
+            break
+        if n % k == 0:
+            n /= k
+        else:
+            n -= 1
+        answer += 1
+    end = time.time()
+    print('solve:', answer)
+    print("Time:", end-start)
 
 if __name__=="__main__":
-    solution()
+    n, k = map(int, input().split())
+    solve(n, k)
