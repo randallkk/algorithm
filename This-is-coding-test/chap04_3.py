1f307b63b508d547d39a7ba03d8b0d35483fb1c6
randallkk <randallkk@sookmyung.ac.kr>
2021-11-07 00:00:31 +0900

    211106

---
+++ This-is-coding-test/chap04_3.py
@@ -0,0 +1,10 @@
+def solve():
+    row, col = map(int, input().split())
+    char = list(map(int, input().split()))
+    world = []
+    for i in range(row):
+        world.append(list(map(int, input().split())))
+        
+
+if __name__ == "__main__":
+    solve()
