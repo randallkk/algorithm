a6e2da2ed19390318bebc96aeb6cabed01a216d0
randallkk <randallkk@sookmyung.ac.kr>
2022-07-11 10:19:57 +0900

    220711

--- baekjoon/1182.py
+++ baekjoon/1182.py
@@ -3,7 +3,7 @@
 from itertools import combinations
 
 answer = 0
-n, s = map(int, input().split())
+n, s = map( int, input().split())
 arr = list(map(int, input().split()))
 for i in range(len(arr)):
     sums = [sum(sub) for sub in combinations(arr, i+1)]
--- baekjoon/9663.py
+++ baekjoon/9663.py
@@ -1,3 +1,27 @@
 # N-Queen gold 4
 # https://www.acmicpc.net/problem/9663
 
+answer = 0
+n = int(input())
+board = [[True] * n for _ in range(n)]  # False랑 0으로 둘 다 풀어보기
+
+
+def placeQueen(board, num):
+    global answer
+    if not num:
+        answer += 1
+        return
+    for j in range(n):
+        if board[n - num][j]:
+            for i in range(n - num + 1, n):
+                board[i][j] = False
+                if 0 <= j - (i - n + num) < n:
+                    board[i][j - (i - n + num)] = False
+                if 0 <= j + (i - n + num) < n:
+                    board[i][j + (i - n + num)] = False
+            placeQueen(board[:], num - 1)
+    return
+
+
+placeQueen(board, n)
+print(answer)
