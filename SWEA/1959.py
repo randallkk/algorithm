86c2e760e14241325e99246cfee3383f1be77e42
randallkk <randallkk@sookmyung.ac.kr>
2022-07-31 00:15:54 +0900

    220731

--- SWEA/1959.py
+++ SWEA/1959.py
@@ -2,22 +2,17 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PpoFaAS4DFAUq&categoryId=AV5PpoFaAS4DFAUq&categoryType=CODE&problemTitle=1959
 
 
-from functools import reduce
-
-def yield_prod(i, arr1, arr2):
-    lambda arr1, arr2: arr1[j]*arr2[i+j] for j in range(n),
-
 def solve(n, m, arr1, arr2):
     answer = 0
     if n > m:
-        for i in range(n-m):
-            reduce(max, , 0)
+        for i in range(n - m + 1):
+            answer = max(answer, sum(map(lambda j: arr1[i + j] * arr2[j], range(m))))
     elif n < m:
-        for i in range(m-n):
-            reduce(max, , 0)
+        for i in range(m - n + 1):
+            answer = max(answer, sum(map(lambda j: arr1[j] * arr2[i + j], range(n))))
     else:
-        for tup in zip(arr1, arr2):
-            answer += tup[0]*tup[1]
+        for i in range(n):
+            answer += arr1[i] * arr2[i]
     return answer
 
 
@@ -26,4 +21,4 @@ for i in range(t):
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    print("#%d",solve(n, m, a, b) %(i+1))
+    print("#%d" %(i + 1), solve(n, m, a, b))
